5244d37eb999402df728214757625287707dd3e1
david nwokedike <miklileza@gmail.com>
2025-09-17 13:51:17 +0100

    fixed bug in send route

--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -13,12 +13,16 @@ export default function Textarea({
   disabled?: boolean;
   value?: string;
 }) {
+  const valueObj: { value?: string; defaultValue?: string } = {};
+  if (value && !disabled) valueObj.value = value;
+  else valueObj.defaultValue = value;
+
   return (
     <div className="w-full relative">
       <textarea
         name={name}
         disabled={disabled}
-        value={value || ""}
+        {...valueObj}
         required
         className={
           "resize-none bg-[#ffffff60] rounded-3xl w-full p-6 min-h-40 max-h-60 text-sm md:text-lg font-medium focus:outline-[var(--accent)] focus:outline-2 outline-offset-4" +
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/messages(.*)",
+  "/send(.*)",
 ]);
 
 // const isDashboardRoute = createRouteMatcher(["/create(.*)", "/send(.*)"]);
